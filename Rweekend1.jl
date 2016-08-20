151bf92ea7614769ae81aa846d06f7820fb7df8d
lawless-m <maht0x0r@xsmail.com>
2016-08-20 14:36:25 -0400

    Const world
    with profile available

--- Rweekend1.jl
+++ Rweekend1.jl
@@ -6,13 +6,13 @@ C++ version hosted at http://goo.gl/9yItEO http://goo.gl/sBih70
 unshift!(LOAD_PATH, ".")
 
 using Vecs: Vec3, unitVector
-using Entities: Entity, Sphere, hitWorld, hitEntity!
+using Entities: World, Entity, Sphere, hitWorld, hitEntity!
 using Materials: Lambertian, Metal, Dielectric
 using Rays: Ray, pointAt
 using Cameras: Camera, shoot
 
-function color(r::Ray, world::Vector{Entity}, depth::Int)
-	h = hitWorld(world, r, 0.001, Inf)
+function color(r::Ray, depth::Int)
+	h = hitWorld(WORLD, r, 0.001, Inf)
 	if h == nothing
 		unit_direction = unitVector(r.direction)
 		t = 0.5(unit_direction.y + 1)
@@ -22,25 +22,25 @@ function color(r::Ray, world::Vector{Entity}, depth::Int)
 	if depth < 50
 		onscreen, scattered, attenuation = Materials.scatter(h.material, r, h)
 		if onscreen
-			return attenuation .* color(scattered, world, depth+1)
+			return attenuation .* color(scattered, depth+1)
 		end
 	end
 	
 	return [0.0, 0.0, 0.0]
 end
 
-function push_random_world!(world::Vector{Entity})
+function push_random_entities!(entities::Vector{Entity})
 	for a in -11:10
 		for b in -11:10
 			choose_mat = rand()
 			center = Vec3(a + 0.9rand(), 0.2, b + 0.9rand())
 			if length(center - Vec3(4.0, 0.2, 0.0)) > 0.9
 				if choose_mat < 0.8
-					push!(world, Sphere(center, 0.2, Lambertian(rand()*rand(), rand()*rand(), rand()*rand())))
+					push!(entities, Sphere(center, 0.2, Lambertian(rand()*rand(), rand()*rand(), rand()*rand())))
 				elseif choose_mat < 0.95
-					push!(world, Sphere(center, 0.2, Metal(0.5(1+rand()), 0.5(1+rand()), 0.5(1+rand()), 0.5rand())))
+					push!(entities, Sphere(center, 0.2, Metal(0.5(1+rand()), 0.5(1+rand()), 0.5(1+rand()), 0.5rand())))
 				else
-					push!(world, Sphere(center, 0.2, Dielectric(1.5)))
+					push!(entities, Sphere(center, 0.2, Dielectric(1.5)))
 				end
 			end
 		end
@@ -48,7 +48,7 @@ function push_random_world!(world::Vector{Entity})
 end
 
 
-world = Entity[
+entities = Entity[
 			Sphere(0,-1000,0, 1000, Lambertian(0.5, 0.5, 0.5))
 			, Sphere(0, 1, 0, 1.0, Dielectric(1.5))
 			, Sphere(-4, 1, 0, 1.0, Lambertian(0.4, 0.2, 0.1))
@@ -56,52 +56,55 @@ world = Entity[
 		]
 
 println("Build world")
-push_random_world!(world)
+push_random_entities!(entities)
 
 const WIDTH = 1200
 const HEIGHT = 800
 const SAMPLES = 10
-const CAMERA = Camera(Vec3(13,2,3), Vec3(0,0,0), Vec3(0,1,0), 20.0, WIDTH/HEIGHT, 0.1, 10.0)
+const WORLD = World(entities, [Camera(Vec3(13,2,3), Vec3(0,0,0), Vec3(0,1,0), 20.0, 3/2, 0.1, 10.0)])
 
-function pixel(world, x::Int, y::Int)
-	r = shoot(CAMERA, (x + rand()) / WIDTH, (y + rand()) / HEIGHT)
-	p = pointAt(r, 2.0)
-	color(r, world, 0)
-end
-
-function render(cols::Matrix)
-	for j in HEIGHT:-1:1 # makes the next line be a countdown rather than up
+function render(cols::Matrix, numsamples::UInt)
+	for j in size(cols)[1]:-1:1 # makes the next line be a countdown rather than up
 		println("Row $j")
-		for i in 1:WIDTH
-			samples = Matrix{Float64}(SAMPLES, 3)
+		for i in 1:size(cols)[2]
+			samples = Matrix{Float64}(numsamples, 3)
 			#@parallel 
-			for s in 1:SAMPLES	
-				samples[s,1:3] = pixel(world, i-1, j-1)
+			for s in 1:numsamples
+				samples[s,1:3] = color(shoot(WORLD.cameras[1], (i-1 + rand()) / size(cols)[2], (j-1 + rand()) / size(cols)[1]), 0) / numsamples
 			end
-			cols[j,i] =  Vec3(sum(samples[1:SAMPLES]), sum(samples[(1+SAMPLES):2SAMPLES]), sum(samples[1+2SAMPLES:3SAMPLES]))
+			cols[j,i] =  Vec3(sum(samples[1:numsamples]), sum(samples[(1+numsamples):2numsamples]), sum(samples[1+2numsamples:3numsamples]))
 		end
 	end
 end
 
 function writepgm(cols::Matrix, filename)
 	pgm = open("$filename.pgm", "w")
-	write(pgm, "P3\n$WIDTH $HEIGHT 255\n")
-	for j in HEIGHT:-1:1
-		for i in 1:WIDTH
-			@printf pgm "%d %d %d\n" [floor(Int,255.99sqrt(v/SAMPLES)) for v in [cols[j,i].x cols[j,i].y cols[j,i].z]]...
+	write(pgm, "P3\n$(size(cols)[2]) $(size(cols)[1]) 255\n")
+	for j in size(cols)[1]:-1:1
+		for i in 1:size(cols)[2]
+			@printf pgm "%d %d %d\n" [floor(Int,255.99sqrt(v)) for v in [cols[j,i].x cols[j,i].y cols[j,i].z]]...
 		end
 	end
 	close(pgm)
 end
 
-function profiled(cols)
-	pixel(world, 50, 50)
-	@profile render(cols)
-	Profile.print()
+if false 
+	cols = Matrix{Vec3}(2*400, 3*400) # height, width
+	render(cols, SAMPLES)
+	writepgm(cols, "Weekend1")
+else
+	if false 
+		cols = Matrix{Vec3}(2*100, 3*100) # height, width
+		color(shoot(WORLD.cameras[1], 50.5/size(cols)[2], 60.5/size(cols)[1]), 0)
+	
+		@profile render(cols, UInt(3))
+		Profile.print()
+	else	
+		cols = Matrix{Vec3}(2*200, 3*200) # height, width
+		color(shoot(WORLD.cameras[1], 50.5/size(cols)[2], 60.5/size(cols)[1]), 0)
+		@time render(cols, UInt(3))
+		#   7.929312 seconds (1.89 G allocations: 70.498 GB, 6.06% gc time)
+	end
+	writepgm(cols, "Profiled")
 end
-
-cols = Matrix{Vec3}(HEIGHT, WIDTH)
-render(cols)
-writepgm(cols, "Weekend1")
-
 

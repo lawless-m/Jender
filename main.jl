283f00b74a9ad896ed776c065e483b46a1b734d3
lawless-m <maht0x0r@xsmail.com>
2016-09-14 21:01:03 -0400

    progressive rendering via CSP
    woo

--- main.jl
+++ main.jl
@@ -11,7 +11,7 @@ using Materials: Material, Lambertian, Metal, Dielectric
 using Rays: Ray, pointAt
 using Cameras: Camera, shoot
 
-function renderPixel(i, j, w, h, numsamples)
+function renderPixel(i::Int, j::Int, w::Int, h::Int, numsamples::Int)
 	c = Float64[0,0,0] 
 	for s in 1:numsamples
 		c += Rays.color(shoot(WORLD.cameras[1], (i-1 + rand()) / w, (j-1 + rand()) / h), 0)
@@ -19,27 +19,29 @@ function renderPixel(i, j, w, h, numsamples)
 	c
 end
 
-function render(cols::Matrix{Vec3}, numsamples::Int)
+function render(w::Int, h::Int, numsamples::Int)
 	refs = Vector{RemoteRef}(numsamples)
 	samples = Vector{Vector{Float64}}(numsamples)
 	
-	for i in 1:size(cols)[2]
-		println("Column $(size(cols)[2]-i)")
-			
-		for j in 1:size(cols)[1] 
-			pixel = renderPixel(i, j, size(cols)[2], size(cols)[1], numsamples)
-			cols[j,i] =  Vec3(pixel[1] / numsamples, pixel[2] / numsamples, pixel[3] / numsamples)
+	for j in h:-1:1
+		println("Row $j")
+		row = Vector{Vec3}(w)
+		for i in 1:w
+			pixel = renderPixel(i, j, w, h, numsamples)
+			row[i] =  Vec3(pixel[1] / numsamples, pixel[2] / numsamples, pixel[3] / numsamples)
 		end
+		produce(row)
 	end
 end
 
-function writepgm(cols::Matrix, filename)
+function writepgm(pipeline, w, h, filename)
 	f(v::Float64) = floor(Int,255.99*sqrt(v))
 	pgm = open("$filename.pgm", "w")
-	write(pgm, "P3\n$(size(cols)[2]) $(size(cols)[1]) 255\n")
-	for j in size(cols)[1]:-1:1
-		for i in 1:size(cols)[2]
-			write(pgm, "$(f(cols[j,i].x)) $(f(cols[j,i].y)) $(f(cols[j,i].z)) ")
+	write(pgm, "P3\n$w $h 255\n")
+	for j in 1:h
+		row = consume(pipeline)
+		for i in 1:w
+			write(pgm, "$(f(row[i].x)) $(f(row[i].y)) $(f(row[i].z)) ")
 		end
 		write(pgm, "\n")
 	end
@@ -59,29 +61,20 @@ println("Build world")
 Entities.push_random_entities!()
 
 function noprofile()
-	cols = Matrix{Vec3}(2*400, 3*400) # height, width
-	@time render(cols, SAMPLES)
-	writepgm(cols, "Weekend1")
+	w, h = 3*400, 2*400
+	writepgm(Task(()->@time render(w, h, SAMPLES)), w, h, "Weekend1")
 end
 
 function profiled()
-	cols = Matrix{Vec3}(2*100, 3*100) # height, width		#addcolor!(shoot(WORLD.cameras[1], 50.5/size(cols)[2], 60.5/size(cols)[1]), 0)
-	render(cols, 3)
-	#Profile.clear_malloc_data() # julia --track-allocation=user main.jl
-	@profile render(cols, 3)
+	w, h = 3*100, 2*100
+	writepgm(Task(()->@profile render(w, h, 3)), w, h, "Profiled1")
 	Profile.print()
-	writepgm(cols, "Profiled")
 end
 
-function timed()
-	cols = Matrix{Vec3}(2*200, 3*200) # height, width
-	@time Rays.color(shoot(WORLD.cameras[1], 50.5/size(cols)[2], 60.5/size(cols)[1]), 0)
-	#println(c) #   Vecs.RGB(0.1087251386964388,0.0434686890557862,0.18448252480043215)
-	#quit()
-	@time render(cols, 3)
-	#@time render(cols, 3)
-	# 42.015203 seconds (1.88 G allocations: 70.071 GB, 8.08% gc time)
-	writepgm(cols, "Profiled")
+function small()
+	w, h = 3*50, 2*50
+	writepgm(Task(()->@time render(w, h, 1)), w, h, "Small1")
 end	
 
-timed()
+noprofile()
+

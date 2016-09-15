8fd5dea6f9a6ea0ecf4ba85315365f096667ecca
lawless-m <maht0x0r@xsmail.com>
2016-09-14 21:29:09 -0400

    make aspect ratio obvious

--- main.jl
+++ main.jl
@@ -3,7 +3,7 @@ C++ version hosted at http://goo.gl/9yItEO http://goo.gl/sBih70
 
 =#
 
-addprocs(4)
+
 unshift!(LOAD_PATH, ".")
 using Vecs: zero, Vec3, unitVector
 using Entities: WORLD, World, pushEntity!, pushCamera!, Entity, Sphere, hitWorld, hitEntity!
@@ -16,7 +16,7 @@ function renderPixel(i::Int, j::Int, w::Int, h::Int, numsamples::Int)
 	for s in 1:numsamples
 		c += Rays.color(shoot(WORLD.cameras[1], (i-1 + rand()) / w, (j-1 + rand()) / h), 0)
 	end
-	c
+	c / numsamples
 end
 
 function render(w::Int, h::Int, numsamples::Int)
@@ -25,12 +25,7 @@ function render(w::Int, h::Int, numsamples::Int)
 	
 	for j in h:-1:1
 		println("Row $j")
-		row = Vector{Vec3}(w)
-		for i in 1:w
-			pixel = renderPixel(i, j, w, h, numsamples)
-			row[i] =  Vec3(pixel[1] / numsamples, pixel[2] / numsamples, pixel[3] / numsamples)
-		end
-		produce(row)
+		produce([Vec3(renderPixel(i, j, w, h, numsamples)) for i in 1:w])
 	end
 end
 
@@ -50,31 +45,33 @@ end
 
 srand(0)
 const SAMPLES = 10
+const ASPECTW = 3
+const ASPECTH = 2
 		
 pushEntity!(Sphere(0,-1000,0, 1000, Lambertian(0.5, 0.5, 0.5)))
 pushEntity!(Sphere(0, 1, 0, 1.0, Dielectric(1.5)))
 pushEntity!(Sphere(-4, 1, 0, 1.0, Lambertian(0.4, 0.2, 0.1)))
 pushEntity!(Sphere(4, 1, 0, 1.0, Metal(0.7, 0.6, 0.5, 0.0)))
-pushCamera!(Camera(Vec3(13,2,3), Vec3(0,0,0), Vec3(0,1,0), 20.0, 3/2, 0.1, 10.0))
+pushCamera!(Camera(Vec3(13,2,3), Vec3(0,0,0), Vec3(0,1,0), 20.0, ASPECTW/ASPECTH, 0.1, 10.0))
 
 println("Build world")
 Entities.push_random_entities!()
 
-function noprofile()
-	w, h = 3*400, 2*400
+function best()
+	w, h = 400ASPECTW, 400ASPECTH # like this so the aspect ratio is obvious
 	writepgm(Task(()->@time render(w, h, SAMPLES)), w, h, "Weekend1")
 end
 
 function profiled()
-	w, h = 3*100, 2*100
+	w, h = 100ASPECTW, 100ASPECTH
 	writepgm(Task(()->@profile render(w, h, 3)), w, h, "Profiled1")
 	Profile.print()
 end
 
 function small()
-	w, h = 3*50, 2*50
+	w, h = 50ASPECTW, 50ASPECTH
 	writepgm(Task(()->@time render(w, h, 1)), w, h, "Small1")
 end	
 
-noprofile()
+best()
 
